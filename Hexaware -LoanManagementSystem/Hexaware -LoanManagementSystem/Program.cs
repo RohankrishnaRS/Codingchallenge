9b90e22840fe216412e4fa4f6b7e2b5fa9c8972b
Rohan Krishna R.S <160414868+RohankrishnaRS@users.noreply.github.com>
2025-06-27 14:20:19 +0530

    Add files via upload

---
+++ Hexaware -LoanManagementSystem/Hexaware -LoanManagementSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hexaware__LoanManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Hexaware -LoanManagementSystem/Hexaware -LoanManagementSystem/entity/CarLoan.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hexaware__LoanManagementSystem.entity
+{
+    internal class CarLoan
+    {
+    }
+}
